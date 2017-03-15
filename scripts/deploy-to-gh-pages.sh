41d71f8667b5d741bedc803e6f58fb4a56f0acc7
Chris Camel <camel.christophe@gmail.com>
2017-03-15 12:09:20 +0100

    improve travis configuration to deploy to github pages
    (minimal implementation)

---
+++ scripts/deploy-to-gh-pages.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+set -o errexit
+
+# initialize target/site
+cd target
+
+rm -rf site
+mkdir site
+
+git clone https://ccamel:$GH_TOKEN@github.com/ccamel/ccamel.github.io.git site
+
+cd site
+
+git config user.email "$USER_EMAIL"
+git config user.name "$USER_NAME"
+
+# add changes
+mkdir -p playgrounds/playground-binding.scala
+rm -rf playgrounds/playground-binding.scala/*
+cp -R ../../dist/* playgrounds/playground-binding.scala/
+
+# deploy
+if ! git diff-index --quiet HEAD --; then
+  git add .
+  git commit -m "update playground-binding.scala showcases"
+
+  # git push --quiet
+else
+  echo "no changes"
+fi
+
+echo "done"
