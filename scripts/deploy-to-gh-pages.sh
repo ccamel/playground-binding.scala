3237c4e3d1e32d21a26ba4b37c9127aabcfc0ec9
Chris Camel <camel.christophe@gmail.com>
2017-04-19 13:53:47 +0200

    fix travis build (once again)

--- scripts/deploy-to-gh-pages.sh
+++ scripts/deploy-to-gh-pages.sh
@@ -9,6 +9,10 @@ git clone https://ccamel:$GH_TOKEN@github.com/ccamel/playground-binding.scala.gi
 
 cd site
 
+git config user.email "$USER_EMAIL"
+git config user.name "$USER_NAME"
+git config push.default simple
+
 # add changes
 cp -R ../dist/* .
 
